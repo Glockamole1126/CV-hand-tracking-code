b85866eec1e8c8c97159ac710ea61576239f4d83
Glockamole1126 <armaanb1126@gmail.com>
2025-08-15 22:16:50 -0600

    i just put the fries in teh bag

--- Camera_to_hand.py
+++ Camera_to_hand.py
@@ -1,7 +1,9 @@
 import cv2  # used to access camera vision
-import serial
-import time
 import mediapipe as mp
+import numpy as np
+import os
+import uuid
+
 
 Mp_hands = mp.solutions.hands
 Mp_drawing = mp.solutions.drawing_utils
@@ -14,26 +16,28 @@ if not webcam.isOpened():
     exit()
 
 
+with Mp_hands.Hands(max_num_hands=4 , min_detection_confidence = 0.8, min_tracking_confidence=0.5) as hands: 
+    while webcam.isOpened():
+        ret, frame = webcam.read()
+        #convert OpenCV BGR (blue,red,green) into RBG for mediapipe to properly access
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
-while True:
-    success, img = webcam.read()
-    #convert OpenCV BGR (blue,red,green) into RBG for mediapipe to properly access
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        frame.flags.writeable = False
+        result = hands.process(frame)
+        frame.flags.writeable = True
 
-    #use the hand tracking on the frame: variable = function.function(minimum hands, min detection confidence)
-    result = Mp_hands.Hands(max_num_hands=2 , min_detection_confidence = 0.5).process(img)
-
-    #convert back to BGR to allow OpenCV to "draw" over image
-    img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
-    # connect the "landmarks" (these are teh joints) to form a skeleton hand
-    if result.multi_hand_landmarks:
-        for hand_landmarks in result.multi_hand_landmarks:
-            Mp_drawing.draw_landmarks(img, hand_landmarks, connections = Mp_hands.HAND_CONNECTIONS)
+        #convert back to BGR to allow OpenCV to "draw" over image
+        frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+        print(result)
+        # connect the "landmarks" (these are teh joints) to form a skeleton hand
+        if result.multi_hand_landmarks:
+            for hand_landmarks in result.multi_hand_landmarks:
+                Mp_drawing.draw_landmarks(frame, hand_landmarks, connections = Mp_hands.HAND_CONNECTIONS)
 
 
-    cv2.imshow('Original Frame', img)
+        cv2.imshow('Original Frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break  # pressing q breaks the loop anf allows for the next commands to run shutting it all down 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break  # pressing q breaks the loop anf allows for the next commands to run shutting it all down 
 webcam.release()
 cv2.destroyAllWindows()
--- Self_Documentation.mg
+++ Self_Documentation.mg
@@ -6,3 +6,7 @@ Useful resources
 log 1 2025/08/12 9:20pm
     - very raw imports and use of code, as of right now it can only track the basic movement, camera is a bit laggy and so i will have to check up on that
     - followed a tutorial for the basic
+
+log 2 2025/08/15 10:14
+    - stightly refined code, it functions at a higher frame rate and runs quicker, it still runs off a previously found tutorial online
+    - neect step is figuring out wtf to do with this
