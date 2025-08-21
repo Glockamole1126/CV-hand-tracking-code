b708c5729476d5f5ee3bacb3880b622285cec841
Glockamole1126 <armaanb1126@gmail.com>
2025-08-20 23:17:18 -0600

    can sense hand from any angle

--- Camera_to_hand.py
+++ Camera_to_hand.py
@@ -3,23 +3,39 @@ import mediapipe as mp
 import numpy as np
 import os
 import uuid
+import math
 
 
 Mp_hands = mp.solutions.hands
 Mp_drawing = mp.solutions.drawing_utils
 
 
-def is_finger_closed(tip, pip):
-    return tip.y > pip.y
+
+
+def distance(a, b):
+    return math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2 + (a.z - b.z)**2)
+
+def finger_closed(hand_landmarks, tip_id, pip_id, wrist_id = 0):
+    tip = hand_landmarks.landmark[tip_id]
+    pip = hand_landmarks.landmark[pip_id]
+    wrist = hand_landmarks.landmark[wrist_id]
+
+    tip_dist = distance(tip, wrist)
+    pip_dist = distance(pip, wrist)
+
+
+    return tip_dist < pip_dist
 
 def hand_closed(hand_landmarks):
+
     finger_tips = [4, 8, 12, 16, 20]   # thumb, index, middle, ring, pinky
-    finger_pips = [3, 6, 10, 14, 18]
+    finger_pips = [3, 6, 10, 14, 18]   # corrected pip indices
+
     closed_count = 0
     for tip, pip in zip(finger_tips, finger_pips):
-        if hand_landmarks.landmark[tip].y > hand_landmarks.landmark[pip].y:
-            closed_count += 1
-    
+        if finger_closed(hand_landmarks, tip, pip):
+            closed_count += 1  
+
     return closed_count == 5
 
 
@@ -53,6 +69,7 @@ with Mp_hands.Hands(max_num_hands=2 , min_detection_confidence = 0.8, min_tracki
                 for id, lm in enumerate(hand_landmarks.landmark):
                     h, w, c, = frame.shape
                     cx, cy = int(lm.x*w), int(lm.y*h)
+                    
                     if hand_closed(hand_landmarks):
                         print("Hand is CLOSED")
                     else:
